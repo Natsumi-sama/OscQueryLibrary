0a71ee97fe49514b0ec675a173d267ff736c0ae5
Luca <luca@perugia.ovh>
2024-09-30 13:32:15 +0200

    Fix oscparameternodeend

--- OscQueryLibrary/Models/JsonSourceGenerators.cs
+++ OscQueryLibrary/Models/JsonSourceGenerators.cs
@@ -8,6 +8,6 @@ namespace OscQueryLibrary.Models;
 [JsonSerializable(typeof(RootNode))]
 [JsonSerializable(typeof(AvatarContents))]
 [JsonSerializable(typeof(OscParameterNode))]
-[JsonSerializable(typeof(OscParameterNodeEnd<>))]
+[JsonSerializable(typeof(OscParameterNodeEnd<string>))] // We need to specify generic type <T> here, otherwise we get a warning about it ignoring this 
 [JsonSourceGenerationOptions(GenerationMode = JsonSourceGenerationMode.Default, WriteIndented = true, AllowTrailingCommas = true)]
 internal partial class ModelsSourceGenerationContext : JsonSerializerContext;
